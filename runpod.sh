dae58c9d09f616181a6d7074c44f16d47aaee85f
ahsanirfan961 <ahsanirfan961@gmail.com>
2024-09-12 23:38:12 +0500

    fix

--- runpod.sh
+++ runpod.sh
@@ -41,10 +41,9 @@ python Synthetic-Data-Techniques/create_config.py \
   --response_model_path="$RESPONSE_MODEL_PATH"
 
 
-ls 
 ls Synthetic-Data-Techniques
 
-cat test.yaml
+cat Synthetic-Data-Techniques/test.yaml
 
 
 # if [ "$DEBUG" == "False" ]; then
