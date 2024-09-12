696c281e81f913204cd4b509ead88688ea5219bf
ahsanirfan961 <ahsanirfan961@gmail.com>
2024-09-12 23:11:22 +0500

    fix path bug

--- runpod.sh
+++ runpod.sh
@@ -30,7 +30,7 @@ if [ -n "$HUGGINGFACE_TOKEN" ]; then
     huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
 fi
 
-python create_config.py \
+python ./create_config.py \
   --hf_token="$HUGGINGFACE_TOKEN" \
   --input_dataset_path="$INPUT_DATASET_PATH" \
   --output_dataset_path="$OUTPUT_DATASET_PATH" \
@@ -41,7 +41,7 @@ python create_config.py \
   --response_model_path="$RESPONSE_MODEL_PATH"
 
 
-cat test.yaml
+cat ./test.yaml
 
 
 # if [ "$DEBUG" == "False" ]; then
